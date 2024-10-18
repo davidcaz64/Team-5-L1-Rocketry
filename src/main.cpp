73bfac4c1d86f4c44acc7aea13fb4e67f85d3f8e
davidcaz64 <davidcazares64@gmail.com>
2024-10-18 01:01:46 -0500

    deleted:    example.cpp 	modified:   platformio.ini 	modified:   src/main.cpp
        Lotta changes, but oh boy it actually works now
        that's mostly due to soldering the BMP280 and
        finally giving the SD card module the 5V it deserves.
        Data integrity and safety focus coming up next!

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <SPI.h>
 #include <SD.h>
 
-// Set pins for I2C comms on ESP32
+// Set pins for I2C comm on ESP32
 #define I2C_SDA 21
 #define I2C_SCL 22
 
@@ -12,7 +13,7 @@
 #define BMP280_I2C_ADDR 0x76
 
 // Chip select pin for SD card
-#define SD_CS 5
+#define chipSelect 5
 
 Adafruit_BMP280 bmp;
 
@@ -23,61 +24,68 @@ void setup()
     // Initialize I2C
     Wire.begin(I2C_SDA, I2C_SCL);
     
-    if (!SD.begin(SD_CS))
-    {
-        Serial.println("SD card initialization failed!");
-        while (1);
-    }
-    Serial.println("SD card initialized succesfully.");
-
     if (!bmp.begin(BMP280_I2C_ADDR))
     {
         Serial.println("Could not find a valid BMP280 senor, check wiring!");
         while (1);
     }
 
-    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
+        bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
                     Adafruit_BMP280::SAMPLING_X2,
                     Adafruit_BMP280::SAMPLING_X16,
                     Adafruit_BMP280::FILTER_X16,
                     Adafruit_BMP280::STANDBY_MS_1000);
-       
+
+    // Initialize SPI
+    SPI.begin();
+    
+    if (!SD.begin(chipSelect))
+    {
+        Serial.println("SD card initialization failed!");
+        while (1);
+    }
+    Serial.println("SD card initialized succesfully.");
 }
 
 void loop()
 {
- float temperature = bmp.readTemperature();
- float pressure = bmp.readPressure();
- float altitude = bmp.readAltitude();
+    float temperature = bmp.readTemperature();
+    float pressure = bmp.readPressure() / 100; // Converts Pa to HPa
+    float altitude = bmp.readAltitude();
  
- Serial.println("Temperature = ");
- Serial.print(temperature);
- Serial.print(" *C");
+    Serial.print("Temperature = ");
+    Serial.print(temperature);
+    Serial.println(" *C");
  
- Serial.println("Pressure = ");
- Serial.print(pressure);
- Serial.print(" hPa");
+    Serial.print("Pressure = ");
+    Serial.print(pressure);
+    Serial.println(" hPa");
  
- Serial.println("Approx. Altitude = ");
- Serial.print(altitude);
- Serial.print(" m");
+    Serial.print("Approx. Altitude = ");
+    Serial.print(altitude);
+    Serial.println(" m");
 
  // SD card module will use SPI comms
- File dataFile = SD.open("sensor_data.txt", FILE_WRITE);
- if (dataFile)
- {
-    dataFile.println("Temperature = ");
-    dataFile.print(temperature);
-    dataFile.print(" *C");
+    File dataFile = SD.open("/sensor_data.txt", FILE_WRITE);
+
+    if (dataFile)
+    {
+        dataFile.print("Temperature = ");
+        dataFile.print(temperature);
+        dataFile.println(" *C");
  
-    dataFile.println("Pressure = ");
-    dataFile.print(pressure);
-    dataFile.print(" hPa");
+        dataFile.print("Pressure = ");
+        dataFile.print(pressure);
+        dataFile.println(" hPa");
  
-    dataFile.println("Approx. Altitude = ");
-    dataFile.print(altitude);
-    dataFile.print(" m");
- }
- 
- delay(2000);
+        dataFile.print("Approx. Altitude = ");
+        dataFile.print(altitude);
+        dataFile.println(" m");
+    }
+    else
+    {
+        Serial.println("error opening sensor_data.txt");
+    }
+
+ delay(1000);
 }
